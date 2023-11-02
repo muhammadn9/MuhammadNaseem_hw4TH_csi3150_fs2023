58467066ac0b69e7c046e37fe16bfd12195227d6
muhammadn9 <20mnaseem@gmail.com>
2023-11-02 18:58:32 -0400

    final push

--- js/buttons.js
+++ js/buttons.js
@@ -5,7 +5,7 @@ toggleButtons.forEach((button, index) => {
   button.addEventListener("click", function () {
     if (additionalInfos[index].style.display === "none") {
       additionalInfos[index].style.display = "block";
-      button.textContent = "Hide Info";
+      button.textContent = "Info";
     } else {
       additionalInfos[index].style.display = "none";
       button.textContent = "Info";
